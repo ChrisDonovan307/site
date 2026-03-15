a6b1b02b370e5970cd10dd243689b88cc7794eff
cdonovan <ctudonovan@gmail.com>
2026-03-15 11:57:36 -0400

    add shadvn svelte and button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	let { data } = $props();
+ 	import { Button } from "$lib/components/ui/button/index.js";
 </script>
 
 <h1 class="text-2xl font-bold mb-4">Home</h1>
@@ -7,7 +8,11 @@
 <p>Here is the content</p>
 
 <ul class="list-disc pl-5">
-	{#each data.projects as { title, content, link }}
+	{#each data.projects as { title, _, link }}
 		<li><a href="{link}" class="text-blue-500 underline hover:text-blue-900" target="_blank">{title}</a></li>
 	{/each}
 </ul>
+
+<p>Here is a button</p>
+
+<Button>Click me</Button>
